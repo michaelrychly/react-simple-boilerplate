bb51c14681c1eb30b1551a92222d6da547161756
michaelrychly <MichaelRychly@gmail.com>
2018-04-19 00:03:04 -0400

    Broadcast message

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,20 @@ import React, {Component} from 'react';
 import ChatBar from "./ChatBar.jsx";
 import Message from "./Message.jsx";
 import MessageList from "./MessageList.jsx";
+const uuidv5 = require('uuid/v5');
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      newMessage: "",
       currentUser: {
         name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-          {
-            id: 1,
-            username: "Bob",
-            content: "Has anyone seen my marbles?",
-          },
-          {
-            id: 2,
-            "username": "Anonymous",
-            "content": "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-          }
-        ]
+      messages: []
       }
   }
 
   componentDidMount(){
+    console.log("componentDidMount in <App/>");
     const chatSocket = new WebSocket("ws://localhost:3001/");
 
     chatSocket.onopen = function (event) {
@@ -33,60 +23,37 @@ class App extends Component {
     };
     this.socket = chatSocket;
 
-      console.log("chatSocket is open", this.socket);
-    //this.socket.onmessage = function (event) {
-    //  console.log("Any event from socket: ", event.data);
-    //}
-    console.log("componentDidMount in <App/>");
-    setTimeout(() => {
-      const newMessage = {id:3,
-                          username: "Michael",
-                          content: "I don't think he lost them all"};
-      const messages = this.state.messages.concat(newMessage);
-      this.setState({messages: messages});
-    }, 500);
-
-  }
-
-  _getID = () => {
-    let count = 0;
-    for (let id in this.state.messages){
-        console.log(id);
-        count += 1;
+    chatSocket.onmessage = (event) => {
+      console.log(" original ", event.data);
+      if (event.data != "Connected to server") {
+        //handle incoming message
+        const state = this.state;
+        const newMessage = {id: Object.values(JSON.parse(event.data))[0],
+                            username: Object.values(JSON.parse(event.data))[1],
+                            content: Object.values(JSON.parse(event.data))[2]};
+        const messages = this.state.messages.concat(newMessage);
+        this.setState({messages: messages});
+      } else {
+        console.log("hmm");
+      }
     }
-    return count;
   }
-
-  _handleInput = (event) => {
-    //event.preventDefault();
-
-    //const state = this.state;
-    //const id = this._getID() + 1;
-    //const newMessage = {id: id,
-    //                    username: "Michael",
-    //                    content: event.target.value};
-    //const messages = this.state.messages.concat(newMessage);
-    this.setState({newMessage: event.target.value});
-    console.log("event trigger else: ", this.state);
-  };
-
-  _handleEnter = (event) => {
+  //adding a new message to the state messages
+  _addMessage = (event, sendMessage) => {
     event.preventDefault();
 
+    console.log("Enter ", sendMessage);
+    console.log("event ", event.target);
+
     const state = this.state;
-    const id = this._getID() + 1;
-    const newMessage = {id: id,
+    //creating new message object with UUID
+    const newMessage = {id: uuidv5(`http://localhost:3000/${new Date().getMilliseconds()}`, uuidv5.DNS),
                         username: "Michael",
-                        content: this.state.newMessage};
+                        content: sendMessage};
     const messages = this.state.messages.concat(newMessage);
-    this.setState({newMessage: '', messages: messages});
-    let name = JSON.stringify(newMessage.username);
-
-    this.socket.send("User " + JSON.stringify(newMessage.username).replace(/"([^"]+(?="))"/g, '$1') +
-     " says " + JSON.stringify(newMessage.content).replace(/"([^"]+(?="))"/g, '$1'));
+    this.setState({messages: messages});
+    this.socket.send(JSON.stringify(newMessage));
   };
-
-  // Send text to all users through the server
 
   render() {
     console.log("Rendering <App/>");
@@ -94,7 +61,7 @@ class App extends Component {
       <div>
         <Navbar />
         <MessageList value={this.state.messages} />
-        <ChatBar value={this.state} onSubmit={this._handleEnter} onChange={this._handleInput}/>
+        <ChatBar value={this.state} addMessage={this._addMessage}/>
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,17 +2,28 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class ChatBar extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      newMessage: ""
+    }
+  }
+
+  _newMessage = (event) => {
+    event.preventDefault();
+    const newMessage = event.target.value;
+    this.setState({newMessage: newMessage});
+  }
   render() {
     console.log("Rendering <ChatBar/>");
     return (
       <footer>
-        <form className="chatbar" onSubmit={this.props.onSubmit}>
+        <form className="chatbar" onSubmit={(event) => {this.props.addMessage(event, this.state.newMessage)}}>
           <input className="chatbar-username" placeholder="Your Name (Optional)"
           defaultValue={this.props.value.currentUser.name}/>
           <input className="chatbar-message" placeholder="Type a message and hit ENTER"
-          type="text" name="message" onChange={this.props.onChange}
-          />
-          <input className="chatbar-enter" type="submit"/>
+          type="text" name="message" onChange={this._newMessage}/>
+          <input className="chatbar-enter" type="submit" />
         </form>
       </footer>
     )
